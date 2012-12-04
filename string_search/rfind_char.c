61729ef9c9090eb3ea3b7aa37887e20d89f2caef
Ian Cordasco <graffatcolmingov@gmail.com>
2012-12-04 10:24:46 -0500

    Make rfind_char faster.
    Fix find arrays in tests.

--- string_search/rfind_char.c
+++ string_search/rfind_char.c
@@ -1,24 +1,15 @@
 #include "../lib/strsearch.h"
 
+/* str must be malloc'd */
 int rfind_char(char *str, char c){
-    int i;
-    int max = -1;
-    int len;
+    int i = -1;
 
-    /*
     if (str) {
         for (i = *(&str + 1) - str - 1; i >= 0 && str[i] != c; i--)
             ;
     }
-    */
 
-    if (str) {
-        len = strlen(str);
-        for (i = 0, max = 0; i < len; i++)
-            if (str[i] == c)
-                max = i;
-    }
-    return max;
+    return i;
 }
 
 /* vim: set ts=4:sw=4:et*/
--- tests/test_string_search.c
+++ tests/test_string_search.c
@@ -1,18 +1,21 @@
 #include "../lib/strsearch.h"
 #include "unittest.h"
+#include <stdlib.h>
 
-char text_to_search[47] = "I didn't know you had a cousin Penelope, Bill!";
-/* Above text courtesy of fortune */
+/* char text_to_search[47] = "I didn't know you had a cousin Penelope, Bill!";
+ */
+
+char *text_to_search;
 
 /* find_string tests */
 int test_string_not_in_string(void){
-    char find[14] = "Not in string";
+    char find[] = "Not in string";
 
     return assert_equal_ints(find_string(text_to_search, find), -1);
 }
 
 int test_string_in_string(void){
-    char find[7] = "didn't";
+    char find[] = "didn't";
 
     return assert_equal_ints(find_string(text_to_search, find), 2);
 }
@@ -22,7 +25,7 @@ int test_equal_strings(void){
 }
 
 int test_string_at_beginning_of_string(void){
-    char find[9] = "I didn't";
+    char find[] = "I didn't";
 
     return assert_equal_ints(find_string(text_to_search, find), 0);
 }
@@ -43,6 +46,10 @@ int test_char_at_beginning_of_string(void){
 int test_library_calls(void){
     int count = 0;
 
+    text_to_search = (char *)malloc(47);
+    strcpy(text_to_search, "I didn't know you had a cousin Penelope, Bill!");
+    /* Above text courtesy of fortune */
+
     /* find_string tests */
     count += test_string_not_in_string();
     count += test_string_in_string();
@@ -53,6 +60,8 @@ int test_library_calls(void){
     count += test_char_not_in_string();
     count += test_char_in_string();
     count += test_char_at_beginning_of_string();
+
+    free(text_to_search);
 
     return count;
 }
